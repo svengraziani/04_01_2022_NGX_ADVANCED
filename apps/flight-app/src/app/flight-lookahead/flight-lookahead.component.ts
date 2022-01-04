efe5e8b91e30bef4e55392dc40783bcd86fcc60f
Sven Graziani <SGraziani@anexia-it.com>
2022-01-04 13:48:51 +0100

    add lookahead lab 01

--- apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
+++ apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-lib';
+import { debounceTime, Observable, switchMap, tap } from 'rxjs';
 
 @Component({
   templateUrl: './flight-lookahead.component.html',
@@ -7,8 +10,34 @@ import { Component } from '@angular/core';
 })
 export class FlightLookaheadComponent {
 
+  control = new FormControl();
+flights$: Observable<Flight[]>
+loading = false;  
+
   constructor(private http: HttpClient) {
+    this.flights$ = 
+            this.control
+                .valueChanges
+                .pipe(
+                    debounceTime(300),
+                    tap(input => this.loading = true),
+                    switchMap(input => this.load(input)),
+                    tap(v => this.loading = false)
+                );
   
   }
+
+  load(from: string):Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
+
+};
 
 }
